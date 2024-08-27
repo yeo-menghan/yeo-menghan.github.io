3f3d7dc25a171546beba0de75426276d8664f04a
yeo-menghan <yeomenghan1989@gmail.com>
2024-08-28 00:53:56 +0800

    update next.config.js, .gitignore and nextjs.yml for output folder ./out

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "dist",  // <=== enables static exports
+  output: "export",  // <=== enables static exports
+  distDir: "out",
   reactStrictMode: true,
 };
 
