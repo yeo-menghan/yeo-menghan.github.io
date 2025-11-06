6891423d2948cf4123c53dd642857e3e970f5997
yeo-menghan <yeomenghan1989@gmail.com>
2025-11-06 23:20:16 +0800

    Add SpeedInsights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -54,7 +55,10 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
